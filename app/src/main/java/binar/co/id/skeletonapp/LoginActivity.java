227e9e90b45687edce8b3225aa84bbfc1376459c
Khanifun Nizar <voennizar@gmail.com>
2018-05-24 14:46:17 +0700

    [voen] add variables for username string and password string

--- app/src/main/java/binar/co/id/skeletonapp/LoginActivity.java
+++ app/src/main/java/binar/co/id/skeletonapp/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
@@ -28,7 +29,11 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void doLogin() {
-        // add validation here, if meet the criteria, call method goToMainMenu(), if not call method showError()
+        EditText etUsername = findViewById(R.id.et_username);
+        EditText etPassword = findViewById(R.id.et_password);
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+        // add validation here, if username is equal your name AND if password is equal 123456, call method goToMainMenu(), if not call method showError()
 
         // end here
     }
